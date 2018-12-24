e86d662d9f7ee44cb859b8b9f8ed21de0c02a7b4
Zinovii Dmytriv <zinovii.dmytriv@verygood.systems>
2018-12-24 18:07:50 +0200

    Added toString and equals/hashcode

--- src/main/java/eu/roboflax/cloudflare/objects/spectrum/OriginDNS.java
+++ src/main/java/eu/roboflax/cloudflare/objects/spectrum/OriginDNS.java
@@ -3,6 +3,7 @@ package eu.roboflax.cloudflare.objects.spectrum;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,6 +11,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
 public class OriginDNS {
 
   @SerializedName("name")
