fa4bd9e2d3e44b8d311aac39ee151a6149af1fa5
robinbraemer <robin.braemer@web.de>
2019-02-16 00:05:44 +0100

    move crypto to objects rename package to customhostname (follow convention)

---
+++ src/main/java/eu/roboflax/cloudflare/objects/crypto/customhostname/Method.java
@@ -0,0 +1,6 @@
+package eu.roboflax.cloudflare.objects.crypto.customhostname;
+
+public enum Method {
+
+  http, email, cname
+}
--- src/main/java/eu/roboflax/cloudflare/objects/crypto/customhostname/SslStatus.java
+++ src/main/java/eu/roboflax/cloudflare/objects/crypto/customhostname/SslStatus.java
@@ -1,4 +1,4 @@
-package eu.roboflax.cloudflare.objects.crypto.customHostname;
+package eu.roboflax.cloudflare.objects.crypto.customhostname;
 
 public enum SslStatus {
   initializing, pending_validation, pending_issuance, pending_deployment, active
