6e89dff66bcae6cfecfe4527f96664d61f81ecba
Zinovii Dmytriv <zdmytriv@gmail.com>
2019-02-15 11:41:31 +0200

    Added one more SSL status

--- src/main/java/eu/roboflax/cloudflare/crypto/customHostname/SslStatus.java
+++ src/main/java/eu/roboflax/cloudflare/crypto/customHostname/SslStatus.java
@@ -1,5 +1,5 @@
 package eu.roboflax.cloudflare.objects.crypto.customHostname;
 
 public enum SslStatus {
-  initializing, pending_validation, pending_deployment, active
+  initializing, pending_validation, pending_issuance, pending_deployment, active
 }
